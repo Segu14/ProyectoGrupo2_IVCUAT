ae19a1855c15fedf49bf50986a89f6453d013211
Rogerfps <rpereira40667@ufide.ac.cr>
2024-04-24 01:16:26 -0600

    login errores

---
+++ Grupo2_DesarrolloWeb/src/main/java/proyectog2/service/CorreoService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyectog2.service;
+
+import jakarta.mail.MessagingException;
+/**
+ *
+ * @author roger
+ */
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
